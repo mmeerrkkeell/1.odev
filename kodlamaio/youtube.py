05e1fd9584a6710fa3c86a0c64a0529fd8679425
mmeerrkkeell <127442610+mmeerrkkeell@users.noreply.github.com>
2023-03-11 02:59:30 +0300

    ödev
    08.03.2023 ilk ödev.

---
+++ kodlamaio/youtube.py
@@ -0,0 +1,14 @@
+"""int : Tam sayı
+float : Ondalıklı sayı
+string: metinsel veya karakter ifadeleri
+boolean : Mantıksal ifadeler (Doğru ve Yanlış)
+Liste (List):[] ile gösterilir değişikliğe açıktır.
+Tuple:()ile gösterilir değişiklik yapılamaz
+Dictionary:sözlük gibi düşünülebilir.bir tanımlanan ve tanımlayıcı vardır."""
+"""user name:
+   adress:"""
+s=int(input("ekran sayısı: "))
+if s==1:
+    print("ekranı ikiye bölünüz")
+else:
+    print("eğitime hazırsınız")
